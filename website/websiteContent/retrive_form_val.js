d7e2443aac4ec7017eec66cfefc856f80e439593
automationbuddycode <45566633+automationbuddycode@users.noreply.github.com>
2018-12-03 21:25:03 +0530

    Add files via upload

---
+++ website/websiteContent/retrive_form_val.js
@@ -0,0 +1,15 @@
+ fname = document.getElementById("fname").value;
+ lname = document.getElementById("lname").value;
+ country = document.getElementById("country").selected;
+ subject = document.getElementById("subject").value;
+ 
+ 
+ 
+function sayHello() {
+   alert("Hello World" + fname);
+}
+
+get_form_val(){
+	
+	
+}
